8266b0d58227c467214aca16e98ee6e372a6c854
Salma Ghariani <salma.ghariani@gmail.com>
2020-05-31 01:04:27 +0100

    Creation de la classe Convention, sa JpaRepository et ses services

--- src/main/java/tn/iit/jee/repository/ConventionRepository.java
+++ src/main/java/tn/iit/jee/repository/ConventionRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tn.iit.jee.models.Convention;
 
 public interface ConventionRepository extends JpaRepository<Convention,Long> {
-
 }
